f83665316f15eadef213f15d753f5fc65298941e
mikeda0122 <38754357+mikeda0122@users.noreply.github.com>
2018-09-14 20:07:08 +0900

    Fix some syntax error for making sequence

--- mod_makegrids.f90
+++ mod_makegrids.f90
@@ -12,7 +12,7 @@ contains
     integer(8) :: i
 
     Astate(1:Anint) = [(Amin + (i-1)*((Aint-Amin)/(Anint-1)), i=1,Anint)]
-    Astate(Anint+1:Anum) = [Aint+i*((Amax - Aint)/Anint), i=1,Anint)]
+    Astate(Anint+1:Anum) = [(Aint+i*((Amax - Aint)/Anint), i=1,Anint)]
 
   end subroutine make_A_1
 
